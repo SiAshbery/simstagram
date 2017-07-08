d56ddd9241be4e890effa710fdd6ddb4ae8fdd2d
Simon Ashbery <si.ashbery@gmail.com>
2017-07-08 22:04:21 +0100

    Adds ability to view likes and like and unlike a photo on the index -Refactors likescontroller to redirect to previous page

--- spec/features/can_like_photos_spec.rb
+++ spec/features/can_like_photos_spec.rb
@@ -2,52 +2,110 @@ require_relative './helpers/photo_feature_helpers_spec'
 
 RSpec.feature "Like a Photo", type: :feature do
 
-  scenario "Likes counter starts at 0 by default" do
-    post_image
-    expect(page).to have_content("0 Likes")
+  describe "Liking on Show" do
+
+    scenario "Likes counter starts at 0 by default" do
+      post_image
+      expect(page).to have_content("0 Likes")
+    end
+
+    scenario "Can Like a photo and like counter increases by one" do
+      post_image
+      click_button("Like Photo")
+      expect(page).not_to have_content("1 Likes")
+      expect(page).to have_content("1 Like")
+    end
+
+    scenario "Counts multiple users likes" do
+      post_image
+      click_button("Like Photo")
+      log_out
+      sign_up("Second_User", "second@user.com")
+      click_link("Show")
+      click_button("Like Photo")
+      log_out
+      sign_up("Third_User", "third@user.com")
+      click_link("Show")
+      click_button("Like Photo")
+      expect(page).to have_content("3 Likes")
+    end
+
+    scenario "Can't Like a photo when not signed in" do
+      post_image
+      log_out
+      click_link("Show")
+      click_button("Like Photo")
+      expect(current_path).to eq("/users/new")
+    end
+
+    scenario "Can't Like a photo more than once" do
+      post_image
+      click_button("Like Photo")
+      expect(page).not_to have_button("Like Photo")
+    end
+
+    scenario "Can unlike a photo" do
+      post_image
+      click_button("Like Photo")
+      expect(page).to have_button("Unlike Photo")
+      click_button("Unlike Photo")
+      expect(page).to have_content("0 Likes")
+    end
   end
 
-  scenario "Can Like a photo and like counter increases by one" do
-    post_image
-    click_button("Like Photo")
-    expect(page).not_to have_content("1 Likes")
-    expect(page).to have_content("1 Like")
-  end
+  describe "Liking on Index" do
 
-  scenario "Counts multiple users likes" do
-    post_image
-    click_button("Like Photo")
-    log_out
-    sign_up("Second_User", "second@user.com")
-    click_link("Show")
-    click_button("Like Photo")
-    log_out
-    sign_up("Third_User", "third@user.com")
-    click_link("Show")
-    click_button("Like Photo")
-    expect(page).to have_content("3 Likes")
-  end
+    scenario "Likes counter starts at 0 by default" do
+      post_image
+      visit("/")
+      expect(page).to have_content("0 Likes")
+    end
 
-  scenario "Can't Like a photo when not signed in" do
-    post_image
-    log_out
-    click_link("Show")
-    click_button("Like Photo")
-    expect(current_path).to eq("/users/new")
-  end
+    scenario "Can Like a photo and like counter increases by one" do
+      post_image
+      visit("/")
+      click_button("Like Photo")
+      expect(page).not_to have_content("1 Likes")
+      expect(page).to have_content("1 Like")
+    end
 
-  scenario "Can't Like a photo more than once" do
-    post_image
-    click_button("Like Photo")
-    expect(page).not_to have_button("Like Photo")
-  end
+    scenario "Counts multiple users likes" do
+      post_image
+      visit("/")
+      click_button("Like Photo")
+      log_out
+      sign_up("Second_User", "second@user.com")
+      click_button("Like Photo")
+      log_out
+      sign_up("Third_User", "third@user.com")
+      click_button("Like Photo")
+      expect(page).to have_content("3 Likes")
+    end
 
-  scenario "Can unlike a photo" do
-    post_image
-    click_button("Like Photo")
-    expect(page).to have_button("Unlike Photo")
-    click_button("Unlike Photo")
-    expect(page).to have_content("0 Likes")
-  end
+    scenario "Can't Like a photo when not signed in" do
+      post_image
+      visit("/")
+      log_out
+      click_link("Show")
+      click_button("Like Photo")
+      expect(current_path).to eq("/users/new")
+    end
 
+    scenario "Can't Like a photo more than once" do
+      post_image
+      visit("/")
+      click_button("Like Photo")
+      expect(page).not_to have_button("Like Photo")
+    end
+
+    scenario "Can unlike a photo" do
+      post_image
+      visit("/")
+      click_button("Like Photo")
+      expect(page).to have_button("Unlike Photo")
+      click_button("Unlike Photo")
+      expect(page).to have_content("0 Likes")
+    end
+
+  end
 end
