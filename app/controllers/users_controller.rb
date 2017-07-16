18eee662cc8062a5df051dc89eaaefd46f829167
Simon Ashbery <si.ashbery@gmail.com>
2017-07-16 11:14:32 +0100

    Users have a profile page -Profile page displays user's name -Added show routes for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,28 @@ class UsersController < ApplicationController
     end
 
     def create
-            user = User.new(user_params)
-        if user.save
-            session[:user_id] = user.id
-            redirect_to '/'
-        else
-            redirect_to '/users/new'
-        end
-    end 
+      @user = User.new(user_params)
+      verify_user_has_saved
+    end
+
+    def show
+      find_user
+    end
 
     private
+
+    def verify_user_has_saved
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to '/'
+      else
+        redirect_to '/users/new'
+      end
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:display_name, :email, :password, :password_confirmation)
