7c821990cb35d6d2e7a77853058760cc62025249
Simon Ashbery <si.ashbery@gmail.com>
2017-07-22 19:21:38 +0100

    Flashes no user error when logging in with invalid email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,14 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
+      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
+      flash[:success] = "Signed in!"
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      assign_error_types(user)
       redirect_to '/session/new'
     end
   end
@@ -20,6 +22,13 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to '/'
+  end
+
+  private
+
+  def assign_error_types(user)
+    flash[:no_user_error] = "User does not exist." unless user
+    # flash[:password_error] = "Incorrect password" unless user.authenticate(params[:password])
   end
 
 
