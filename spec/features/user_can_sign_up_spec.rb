ef966c221d9cb986cd06d9ffd3ff948eda8fd350
Simon Ashbery <si.ashbery@gmail.com>
2017-07-22 14:43:31 +0100

    Flashes no name error

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature "User Signup", type: :feature do
 
     scenario "Flashes a successful sign up message" do
       sign_up
-      expect(page).to have_content("success: You're All Signed Up!")
+      expect(page).to have_content("success: You're all signed up!")
     end
 
   end
@@ -27,14 +27,23 @@ RSpec.feature "User Signup", type: :feature do
 
       scenario "Flashes a passwords don't match error message" do
         sign_up("Test_User", "test@email.com", "password", "wrongpassword")
-        expect(page).to have_content("error: Your Passwords Don't Match")
+        expect(page).to have_content("confirm_password_error: Your passwords don't match.")
       end
 
     end
 
-    scenario "Can't sign up without a name" do
-      sign_up(nil, "test@email.com", "password", "wrongpassword")
-      expect(current_path).to eq("/users/new")
+    describe "No Name" do
+
+      scenario "Can't sign up without a name" do
+        sign_up(nil, "test@email.com", "password", "wrongpassword")
+        expect(current_path).to eq("/users/new")
+      end
+
+      scenario "Flashes a no name error" do
+        sign_up(nil, "test@email.com", "password", "password")
+        expect(page).to have_content("no_name_error: You must enter a name.")
+      end
+
     end
 
     scenario "Can't sign up without an email" do
@@ -47,4 +56,5 @@ RSpec.feature "User Signup", type: :feature do
       expect(current_path).to eq("/users/new")
     end
   end
+
 end
