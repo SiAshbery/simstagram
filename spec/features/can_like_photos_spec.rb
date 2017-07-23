a4dace65320a3796e38a5413e1ffa83d9d237cf9
Simon Ashbery <si.ashbery@gmail.com>
2017-07-23 20:42:31 +0100

    First UI pass

--- spec/features/can_like_photos_spec.rb
+++ spec/features/can_like_photos_spec.rb
@@ -11,22 +11,22 @@ RSpec.feature "Like a Photo", type: :feature do
 
     scenario "Can Like a photo and like counter increases by one" do
       post_image
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).not_to have_content("1 Likes")
       expect(page).to have_content("1 Like")
     end
 
     scenario "Counts multiple users likes" do
       post_image
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       log_out
       sign_up("Second_User", "second@user.com")
       click_link("Show")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       log_out
       sign_up("Third_User", "third@user.com")
       click_link("Show")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).to have_content("3 Likes")
     end
 
@@ -34,21 +34,21 @@ RSpec.feature "Like a Photo", type: :feature do
       post_image
       log_out
       click_link("Show")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(current_path).to eq("/users/new")
     end
 
     scenario "Can't Like a photo more than once" do
       post_image
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).not_to have_button("Like Photo")
     end
 
     scenario "Can unlike a photo" do
       post_image
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).to have_button("Unlike Photo")
-      click_button("Unlike Photo")
+      first(".edit_like").click_button("Unlike Photo")
       expect(page).to have_content("0 Likes")
     end
   end
@@ -64,7 +64,7 @@ RSpec.feature "Like a Photo", type: :feature do
     scenario "Can Like a photo and like counter increases by one" do
       post_image
       visit("/")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).not_to have_content("1 Likes")
       expect(page).to have_content("1 Like")
     end
@@ -72,13 +72,13 @@ RSpec.feature "Like a Photo", type: :feature do
     scenario "Counts multiple users likes" do
       post_image
       visit("/")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       log_out
       sign_up("Second_User", "second@user.com")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       log_out
       sign_up("Third_User", "third@user.com")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).to have_content("3 Likes")
     end
 
@@ -87,21 +87,21 @@ RSpec.feature "Like a Photo", type: :feature do
       visit("/")
       log_out
       click_link("Show")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(current_path).to eq("/users/new")
     end
 
     scenario "Can't Like a photo more than once" do
       post_image
       visit("/")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).not_to have_button("Like Photo")
     end
 
     scenario "Can unlike a photo" do
       post_image
       visit("/")
-      click_button("Like Photo")
+      first(".new_like").click_button("Like Photo")
       expect(page).to have_button("Unlike Photo")
       click_button("Unlike Photo")
       expect(page).to have_content("0 Likes")
