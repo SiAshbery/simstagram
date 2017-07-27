d1d69e1f9df73a48b1b62a0f159c5bf940ca9b7b
Simon Ashbery <si.ashbery@gmail.com>
2017-07-27 15:00:06 +0100

    Changes carrierwave uploader to use file system for test uploads

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,10 @@
 class PhotoUploader < CarrierWave::Uploader::Base
 
-  storage :fog
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
